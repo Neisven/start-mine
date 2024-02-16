54d26e79e0ef10201d484f9c74e15da9046a2948
Neisven <156550353+Neisven@users.noreply.github.com>
2024-02-16 10:25:49 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
 echo "start"
 sleep 5
-pwds="~/neisven-start-mine/"
+sudo apt update && sudo apt install -y curl default-jdk 
+# install deps ^^
+pwds=~/neisven-start-mine/
 mkdir neisven-start-mine
 curl -O https://raw.githubusercontent.com/Neisven/start-mine/main/mine.sh
-cp mine.sh $pwds
+curl -O https://raw.githubusercontent.com/Neisven/start-mine/main/mine.jar
+mv mine.sh $pwds
+mv mine.jar $pwds
 chmod +x $pwds/mine.sh
 export PATH="$PATH:$pwds"
-mine.sh
+$pwds/mine.sh
