3d3f317b6af5dbbcc65e80ab8ca642a8c1b8e659
Neisven <156550353+Neisven@users.noreply.github.com>
2024-02-16 10:42:08 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,27 @@
-echo "start"
-sleep 5
-sudo apt update && sudo apt install -y curl default-jdk 
-# install deps ^^
-pwds=~/neisven-start-mine/
-mkdir neisven-start-mine
-curl -O https://raw.githubusercontent.com/Neisven/start-mine/main/mine.sh
-curl -O https://raw.githubusercontent.com/Neisven/start-mine/main/mine.jar
-mv mine.sh $pwds
-mv mine.jar $pwds
-chmod +x $pwds/mine.sh
-export PATH="$PATH:$pwds"
-mine.sh
-echo "enter command: mine.sh for execute TL."
-rm install.sh
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+print "start\n";
+sleep 5;
+system("sudo apt update && sudo apt install -y curl default-jdk");
+
+my $pwds = "$ENV{HOME}/neisven-start-mine/";
+mkdir $pwds unless -d $pwds;
+chdir $pwds or die "Cannot change directory to $pwds: $!";
+
+system("curl -O https://raw.githubusercontent.com/Neisven/start-mine/main/mine.sh");
+system("curl -O https://raw.githubusercontent.com/Neisven/start-mine/main/mine.jar");
+
+rename "mine.sh", "$pwds/mine.sh" or die "Cannot move mine.sh: $!";
+rename "mine.jar", "$pwds/mine.jar" or die "Cannot move mine.jar: $!";
+
+chmod 0755, "$pwds/mine.sh";
+
+$ENV{PATH} = "$ENV{PATH}:$pwds";
+system("$pwds/mine.sh");
+print "enter command: mine.sh for execute TL.\n";
+unlink "install.sh";
+
+system("$pwds/mine.sh");
